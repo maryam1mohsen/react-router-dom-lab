c8251afb948fbd63fdf74ab2e94e802be21a8d0a
Maryam <maryammohsen202002861@gmail.com>
2024-08-28 09:50:31 +0300

    done

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import MailboxForm from './components/MailboxForm';
+import MailboxList from './components/MailboxList';
+import MailboxDetails from './components/MailboxDetails';
+
+function App() {
+  const [mailboxes, setMailboxes] = useState([]);
+
+  const addBox = ({ boxholder, boxSize }) => {
+    const newMailbox = {
+      _id: mailboxes.length + 1,
+      boxholder,
+      boxSize,
+    };
+    setMailboxes([...mailboxes, newMailbox]);
+  };
+
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={<main><h1>Post Office</h1></main>}
+        />
+        <Route
+          path="/mailboxes"
+          element={<MailboxList mailboxes={mailboxes} />}
+        />
+        <Route
+          path="/new-mailbox"
+          element={<MailboxForm addBox={addBox} />}
+        />
+        <Route
+          path="/mailboxes/:mailboxId"
+          element={<MailboxDetails mailboxes={mailboxes} />}
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
